0881418bd8696ae55272ed1730fed5cc8943eba7
Siddhant Agnihotri <siddhant.ag7@gmail.com>
2020-06-21 03:29:28 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [isLoading, setLoader] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {isLoading ? <Text>Fetching the Weather</Text> : <Text>Weather App</Text>}
     </View>
   );
 }
@@ -12,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
